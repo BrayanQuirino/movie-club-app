775de533be921bb320fb6ca0bfc35f1fd257b355
BrayanQuirino <bq.acatlan@gmail.com>
2026-03-30 20:56:35 -0600

    perfect blue

--- src/reward.jsx
+++ src/reward.jsx
@@ -6,28 +6,14 @@ const Reward = ({title,title_content, description,
   return (
     <>
     <div className="notification-container">
-      <h2 className="notification-title">Lider de Temporada</h2>
       <h2 className="notification-title">Recompensa: <a className="focus">Regalo especial</a></h2>
       <h2 className="notification-title">Costo: <a className="focus">Todos los puntos</a></h2>
       <h2 className="notification-title">Aplicable a: <a className="focus">Líder de puntos al final de temporada 23 de mayo.</a></h2>
     </div>
     <div className="notification-container">
-      <h2 className="notification-title">Lider de Puntos</h2>
-      <h2 className="notification-title">Recompensa: <a className="focus">Puedes desestimar 2 generos de la proxima película a elegir o desestimar la película elegida</a></h2>
-      <h2 className="notification-title">Costo: <a className="focus">Mitad de puntos</a></h2>
-      <h2 className="notification-title">Aplicable a: <a className="focus">Líder de puntos</a></h2>
-    </div>
-    <div className="notification-container">
-      <h2 className="notification-title">Lider de Puntos</h2>
-      <h2 className="notification-title">Recompensa: <a className="focus">Puedes elegir la proxima pelicula a cambio de todos tus puntos si eres el lider</a></h2>
+      <h2 className="notification-title">Recompensa: <a className="focus">Puedes elegir la proxima pelicula a cambio de todos tus puntos</a></h2>
       <h2 className="notification-title">Costo: <a className="focus">Todos tus puntos</a></h2>
-      <h2 className="notification-title">Aplicable a: <a className="focus">Líder de puntos</a></h2>
-    </div>
-    <div className="notification-container">
-      <h2 className="notification-title">Quitar un genero</h2>
-      <h2 className="notification-title">Descripción: <a className="focus">Puedes quitar un genero de la proxima película a elegir</a></h2>
-      <h2 className="notification-title">Costo: <a className="focus">2 puntos</a></h2>
-      <h2 className="notification-title">Aplicable a: <a className="focus">Cualquiera</a></h2>
+      <h2 className="notification-title">Aplicable a: <a className="focus">Todos</a></h2>
     </div>
     </>
   );
